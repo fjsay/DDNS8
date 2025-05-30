448ac950fecc248c49b94b075c2c4eeaf91c8e05
FJSAY <75113871+fjsay@users.noreply.github.com>
2025-05-30 14:46:14 +0800

    Update ddns_v4.bat

--- windows/ddns_v4.bat
+++ windows/ddns_v4.bat
@@ -6,7 +6,7 @@ echo 正在更新DDNS记录... [%date% %time%]
 
 curl -s "https://ddns8.cn/api/ddnsapi.php?token=你的API令牌&domain=你的域名"
 
-echo 更新完成！下一次更新将在5分钟后进行...
-timeout /t 300 /nobreak >nul
+echo 更新完成！下一次更新将在1分钟后进行...
+timeout /t 60 /nobreak >nul
 
 goto start
